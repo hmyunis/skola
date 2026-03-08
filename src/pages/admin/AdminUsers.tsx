cea2c955e2813f771b08bd0dfa693cce12f4f020
hmyunis <hamdim801@gmail.com>
2026-03-08 09:40:53 +0000

    Preceding changes

--- src/pages/admin/AdminUsers.tsx
+++ src/pages/admin/AdminUsers.tsx
@@ -5,6 +5,16 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import {
   Select,
   SelectContent,
   SelectItem,
