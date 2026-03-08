8008a146f7932fa483fad50432aedef2ac4ccdf8
hmyunis <hamdim801@gmail.com>
2026-03-08 09:44:17 +0000

    Toast theme integration added

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -8,7 +8,7 @@ export function Toaster() {
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast key={id} {...props} className="border-l-4 border-l-primary">
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && <ToastDescription>{description}</ToastDescription>}
