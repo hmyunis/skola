6d32e27540de5f61cddc998c96e8c4578b2ec52c
hmyunis <hamdim801@gmail.com>
2026-03-08 21:14:46 +0000

    Add file upload to resources

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useRef, useCallback } from "react";
 import { useQuery } from "@tanstack/react-query";
 import {
   fetchResources,
